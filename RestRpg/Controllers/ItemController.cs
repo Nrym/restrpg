2bdf7b759d4f57614b7bcdbed4faa52a36f11e1a
Nrym <realnrym@outlook.com>
2023-09-12 10:56:06 -0500

    On memory cud

--- RestRpg/Controllers/ItemController.cs
+++ RestRpg/Controllers/ItemController.cs
@@ -9,8 +9,9 @@ namespace RestRpg.Controllers
     [ApiController]
     public class ItemController : ControllerBase
     {
-        private List<Item> items = new List<Item>()
+        private static List<Item> items = new List<Item>()
         {
+            new Item() { Id = 0,Name="Item 0",Price=0.99},
             new Item() { Id = 1,Name="Item 1",Price=1.99},
             new Item() { Id = 2,Name="Item 2",Price=4.99},
             new Item() { Id = 3,Name="Item 3",Price=9.99}
@@ -20,6 +21,24 @@ namespace RestRpg.Controllers
         public IEnumerable<Item> Get()
         {
             return items;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Item item)
+        {
+            items.Add(item);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Item item)
+        {
+            items[id] = item;
+        }
+
+        [HttpDelete("{id}")]
+        public void Put(int id)
+        {
+            items.RemoveAt(id);
         }
     }
 }
